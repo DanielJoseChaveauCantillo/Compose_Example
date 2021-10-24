ec2233b56561d1ced7dd71855c29a3aceee62428
Daniel Jose Chaveau Cantillo <danielchaveau@gmail.com>
2021-10-24 18:16:20 -0500

    Proyecto en blanco

---
+++ app/app/src/main/java/com/example/compose_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.compose_example
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_example.ui.theme.Compose_ExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
